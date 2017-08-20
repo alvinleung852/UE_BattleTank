655289b7d292404cffec8bc5a92c19831c8507b2
Alvin Leung <alvin.ho.leung@gmail.com>
2017-08-20 15:24:51 -0400

    get crosslocation pixel location

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -18,7 +18,7 @@ void ATankPlayerController::BeginPlay() {
 
 void ATankPlayerController::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
-
+	AimTowardsCrosshair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const {
@@ -27,5 +27,22 @@ ATank* ATankPlayerController::GetControlledTank() const {
 
 void ATankPlayerController::AimTowardsCrosshair() {
 	if (!GetControlledTank()) { return; }
+	
+	FVector HitLocation;		// OUT parameter
 
+	if (GetSightRayHitLocation(HitLocation)) {
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *HitLocation.ToString());
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector &HitLocation) const {
+	//Find crosshair position in pixel location
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	//FVector HitLocation;
+	FRotator HitRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(HitLocation, HitRotation);
+	return true;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -19,9 +19,15 @@ public:
 	void BeginPlay() override;
 	void Tick(float DeltaSeconds) override;
 
-	ATank* GetControlledTank() const;
-
 private:
 
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5f;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333f;
+
+	ATank* GetControlledTank() const;
+
 	void AimTowardsCrosshair();
+	bool GetSightRayHitLocation(FVector &HitLocation) const;
 };
